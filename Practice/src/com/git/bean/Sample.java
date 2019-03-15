f39e330ad0571cf42a5b98e933aec9d15f1885b1
rajendra gorantla <rajendra.gorantla@s-force.org>
2019-03-15 16:37:38 +0530

    add

---
+++ Practice/src/com/git/bean/Sample.java
@@ -0,0 +1,8 @@
+package com.git.bean;
+
+public class Sample {
+	public void getMessage(String message) {
+		System.out.println("message"+message);
+	}
+
+}
---
+++ Practice/src/com/git/test/SampleTest.java
@@ -0,0 +1,11 @@
+package com.git.test;
+
+import com.git.bean.Sample;
+
+public class SampleTest {
+	public static void main(String[] args) {
+		Sample sample=new Sample();
+		sample.getMessage("git commands");
+	}
+
+}
